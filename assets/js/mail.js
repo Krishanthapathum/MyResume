327cd1d915cb3bcc28a421871253bd06d056b8c3
Krishanthapathum <87825292+Krishanthapathum@users.noreply.github.com>
2023-11-22 02:43:02 +0530

    Modified hosting

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,13 +1,25 @@
 // Your web app's Firebase configuration
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBpHkxDIKfF-bXYT2wcB8pi1i6tifn1m8o",
+//     authDomain: "contact-form-myportfolio-19aac.firebaseapp.com",
+//     databaseURL: "https://contact-form-myportfolio-19aac-default-rtdb.firebaseio.com",
+//     projectId: "contact-form-myportfolio-19aac",
+//     storageBucket: "contact-form-myportfolio-19aac.appspot.com",
+//     messagingSenderId: "862806211114",
+//     appId: "1:862806211114:web:ca10d5d2696a4d31e22153"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBpHkxDIKfF-bXYT2wcB8pi1i6tifn1m8o",
-    authDomain: "contact-form-myportfolio-19aac.firebaseapp.com",
-    databaseURL: "https://contact-form-myportfolio-19aac-default-rtdb.firebaseio.com",
-    projectId: "contact-form-myportfolio-19aac",
-    storageBucket: "contact-form-myportfolio-19aac.appspot.com",
-    messagingSenderId: "862806211114",
-    appId: "1:862806211114:web:ca10d5d2696a4d31e22153"
-};
+    apiKey: "AIzaSyAbRbpzbUUzqBzaq4cTo9QPvLRkpuFWFAM",
+    authDomain: "krishantha-pathum.firebaseapp.com",
+    databaseURL: "https://krishantha-pathum-default-rtdb.firebaseio.com",
+    projectId: "krishantha-pathum",
+    storageBucket: "krishantha-pathum.appspot.com",
+    messagingSenderId: "965403865719",
+    appId: "1:965403865719:web:478ae0b48440fb5e0b8c72",
+    measurementId: "G-N42KRMXCL0"
+  };
 
 // initialize firebase
 firebase.initializeApp(firebaseConfig);
